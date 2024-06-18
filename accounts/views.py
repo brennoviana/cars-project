f0be277d31f7fab3e8320e977528858fed871723
brennoviana <brennovianacoelho@gmail.com>
2024-06-18 11:39:57 -0300

    commit inicial

---
+++ accounts/views.py
@@ -0,0 +1,23 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, FormView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login, authenticate
+from .forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    authentication_form = AuthenticationForm
+
+class UserRegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'accounts/register.html'
+    # success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        valid = super(UserRegisterView, self).form_valid(form)
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(self.request, user)
+        return valid
