ce44c739919110436dd94adcc2e6b0773b40df8e
brennoviana <brennovianacoelho@gmail.com>
2024-06-18 12:46:18 -0300

    commit inicial

--- CarsProject/settings.py
+++ CarsProject/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'middleware.authentication_middleware.LoginRequiredMiddleware',
-
 ]
 
 ROOT_URLCONF = 'CarsProject.urls'
