e87534c6afca0787c5f96bf871c5b22ffac6d59f
brennoviana <brennovianacoelho@gmail.com>
2024-06-18 15:04:52 -0300

    commit inicial

--- middleware/authentication_middleware.py
+++ middleware/authentication_middleware.py
@@ -5,9 +5,17 @@ from django.urls import resolve
 class LoginRequiredMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-
+        self.exempt_urls = ['login', 'logout', 'register']
+        
     def __call__(self, request):
-        if not request.user.is_authenticated and not resolve(request.path_info).url_name in ['login', 'logout', 'register'] and not request.path.startswith('/static/'):
-            return redirect(settings.LOGIN_URL)
+        path = request.path_info
+        print(path)
+        if not request.user.is_authenticated:
+            if path == '/':
+                return redirect(settings.LOGIN_URL)
+            if (not any(path.startswith(url) for url in ['/static/', '/admin/']) and 
+                resolve(path).url_name not in self.exempt_urls):
+                return redirect(settings.LOGIN_URL)
+
         response = self.get_response(request)
         return response
